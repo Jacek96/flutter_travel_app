eea2c88f6634e176c69e9cf7658096faeda8683e
Jacek ferdynus <jacekferdynus96@gmail.com.com>
2023-11-13 17:37:10 +0100

    Add onTap function to NavBar

--- lib/pages/navpages/main_page.dart
+++ lib/pages/navpages/main_page.dart
@@ -11,16 +11,30 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  List pages = [HomePage(), SearchPage(), MyPage()];
+  List pages = [
+    HomePage(),
+    SearchPage(),
+    MyPage(),
+  ];
+  int currentIndex = 0;
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: pages[0],
       bottomNavigationBar: BottomNavigationBar(
+        onTap: onTap,
+        currentIndex: currentIndex,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.grey,
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        elevation: 0,
         items: const [
           BottomNavigationBarItem(
             label: 'Home',
