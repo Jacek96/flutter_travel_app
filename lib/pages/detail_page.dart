61dec11072f01df6f1b1e58a15819f412d5cbfc1
Jacek ferdynus <jacekferdynus96@gmail.com.com>
2023-12-11 19:06:45 +0100

    Add top text

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_app/widgets/app_large_text.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -17,29 +18,64 @@ class _DetailPageState extends State<DetailPage> {
         child: Stack(
           children: [
             Positioned(
+              left: 0,
+              right: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 350,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/view.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
               left: 20,
-              top: 70,
+              top: 50,
               child: Row(
                 children: [
                   IconButton(
                     onPressed: () {},
                     icon: Icon(Icons.menu),
+                    color: Colors.white,
                   ),
                 ],
               ),
             ),
             Positioned(
-                left: 0,
-                right: 0,
-                child: Container(
-                  width: double.maxFinite,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('images/view.jpg'),
-                    ),
+              top: 320,
+              child: Container(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 30),
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
                   ),
-                ))
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ApplargeText(
+                          text: "Szwajcaria",
+                          color: Colors.black87,
+                        ),
+                        ApplargeText(
+                          text: "1600 zł",
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
