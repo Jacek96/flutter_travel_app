8d1a49a8c81275705be9262c8aba3394a8e07be5
Jacek ferdynus <jacekferdynus96@gmail.com.com>
2023-12-21 18:40:19 +0100

    Add scrollable functionality to column

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -61,113 +61,126 @@ class _DetailPageState extends State<DetailPage> {
                     topRight: Radius.circular(30),
                   ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ApplargeText(
-                          text: "Szwajcaria",
-                          color: Colors.black87,
-                        ),
-                        ApplargeText(
-                          text: "1600 zł",
-                          color: Colors.grey,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.location_on,
-                          color: Colors.red,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        AppText(
-                          text: "Zermatt",
-                          color: Colors.black87,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: [
-                        Wrap(
-                          children: List.generate(
-                            5,
-                            (index) {
-                              return Icon(
-                                Icons.star,
-                                color: index < gottenStars
-                                    ? Colors.amber
-                                    : Colors.grey,
-                              );
-                            },
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ApplargeText(
+                            text: "Szwajcaria",
+                            color: Colors.black87,
                           ),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        AppText(
-                          text: "(4.0)",
-                          color: Colors.black87,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 25,
-                    ),
-                    ApplargeText(
-                      text: 'Liczba osób',
-                      color: Colors.black87.withOpacity(0.8),
-                      size: 25,
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    AppText(text: 'Wybierz ilość uczestników'),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Wrap(
-                      children: List.generate(
-                        5,
-                        (index) {
-                          return InkWell(
-                            onTap: () {
-                              setState(() {
-                                selectedIndex = index;
-                              });
-                            },
-                            child: Container(
-                              margin: const EdgeInsets.only(right: 10),
-                              child: AppButtons(
-                                size: 50,
-                                color: selectedIndex == index
-                                    ? Colors.white
-                                    : Colors.black,
-                                backgroundColor: selectedIndex == index
-                                    ? Colors.black
-                                    : Color.fromARGB(255, 226, 210, 210),
-                                borderColor: selectedIndex == index
-                                    ? Colors.black
-                                    : Colors.white,
-                                text: (index + 1).toString(),
-                              ),
-                            ),
-                          );
-                        },
+                          ApplargeText(
+                            text: "1600 zł",
+                            color: Colors.grey,
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.red,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          AppText(
+                            text: "Zermatt",
+                            color: Colors.black87,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Wrap(
+                            children: List.generate(
+                              5,
+                              (index) {
+                                return Icon(
+                                  Icons.star,
+                                  color: index < gottenStars
+                                      ? Colors.amber
+                                      : Colors.grey,
+                                );
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          AppText(
+                            text: "(4.0)",
+                            color: Colors.black87,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      ApplargeText(
+                        text: 'Liczba osób',
+                        color: Colors.black87.withOpacity(0.8),
+                        size: 25,
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      AppText(text: 'Wybierz ilość uczestników'),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) {
+                            return InkWell(
+                              onTap: () {
+                                setState(() {
+                                  selectedIndex = index;
+                                });
+                              },
+                              child: Container(
+                                margin: const EdgeInsets.only(right: 10),
+                                child: AppButtons(
+                                  size: 50,
+                                  color: selectedIndex == index
+                                      ? Colors.white
+                                      : Colors.black,
+                                  backgroundColor: selectedIndex == index
+                                      ? Colors.black
+                                      : Color.fromARGB(255, 226, 210, 210),
+                                  borderColor: selectedIndex == index
+                                      ? Colors.black
+                                      : Colors.white,
+                                  text: (index + 1).toString(),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ApplargeText(
+                        text: 'Opis',
+                        color: Colors.black.withOpacity(0.8),
+                        size: 20,
+                      ),
+                      AppText(
+                          text:
+                              'Szwajcaria to państwo kojarzące się przede wszystkim z ośnieżonymi stokami górskimi, szumiącymi wodospodami, a także licznymi jeziorami z unoszącą się nad taflą wody mgłą. Szwajcaria charakteryzuje się różnorodnością językową. Istnieją, aż cztery języki urzędowe. Jest to kraj bardzo drogi, a miasto Zurych jest czwartym z najdroższych na świecie. Mimo to kraj ten przyciąga odwiedzających, a turystyka stanowi jedną z najważniejszych gałęzi szwajcarskiej gospodarki.'),
+                    ],
+                  ),
                 ),
               ),
             )
